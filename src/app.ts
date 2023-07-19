92c76e9f69381cd9941eced9ff5e1e19115c1a20
aleviannaf <alexandrevianna511@gmail.com>
2023-07-19 16:16:20 -0400

    Update: update in config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "express-async-errors";
 import express, { Application, json } from "express";
 import middlewares from "./middlewares";
+import { userRouter } from "./routers";
 
 const app: Application = express()
 app.use(json())
+
+app.use("/users", userRouter)
 
 app.use(middlewares.handleError);
 export { app }
